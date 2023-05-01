df60725b8e42d9b65f57b6bc02d0dc20361d9e3d
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-05-01 09:01:57 +0300

    first commit

---
+++ assesment.js
@@ -0,0 +1,37 @@
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+function getAvailableBooks(books){
+    booksArgetAvailableBooksay={};
+    for(book in books){
+    const availablebooks=bookarray.forEach( (book)=> {
+    book.push(...book) 
+    });
+
+}
+
+
+}
+console.log(getAvailableBooks(books))
+
+
+
+function getBooksByAuthor(Salinger){
+    author={};
+    const arrayBooks=getBooksByAuthor.filter((book)=>book.title);
+    const author= getBooksByAuthor.map((title)=>book.author);
+}
+console.log(getBooksByAuthor(books))
+
+function addNewBook(book){
+
+
+}
